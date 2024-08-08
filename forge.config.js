49e9cd6c5ee4bf177e3a7418f9aa6f998910551b
Zhiyuan Xu <william@zerodots.net>
2024-08-08 18:29:22 +0800

    完成macOS编译配置

--- forge.config.js
+++ forge.config.js
@@ -1,9 +1,11 @@
 const { FusesPlugin } = require('@electron-forge/plugin-fuses');
 const { FuseV1Options, FuseVersion } = require('@electron/fuses');
+const path = require('path')
 
 module.exports = {
   packagerConfig: {
     asar: true,
+    icon: path.join(__dirname, 'src/assets/icons/icon')
   },
   rebuildConfig: {},
   makers: [
