9c8e08c9dbde3f7be3394e3f740deb457a5ae9be
zerodots <william@zerodots.net>
2024-08-08 21:10:05 +0800

    将跳转窗口更改为960x720

--- src/pages/js/index.js
+++ src/pages/js/index.js
@@ -5,7 +5,7 @@ document,addEventListener('DOMContentLoaded', () => {
     button.addEventListener('click', () => {
         const url = "http://" + input.value
         if(url){
-            window.resizeTo(800, 600)
+            window.resizeTo(960,720)
             window.location.href = url
         }
     })
